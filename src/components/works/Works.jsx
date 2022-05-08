70564700c289a84077b069bc724aa9fe0e1dbda4
Halil <78084765+halilkaran@users.noreply.github.com>
2022-05-08 13:11:57 +0200

    v2.1

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,35 +1,79 @@
 import "./works.scss";
-import mobile from "../../assets/mobile.png";
-import blog from "../../assets/BlogApp.png";
-const works = () => {
+ 
+import arrow from "../../assets/arrow.png";
+import { useState } from "react";
+ 
+const Works = () => {
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const data = [
+    {
+      id: "1",
+      icon: "../../assets/mobile.png",
+      title: "Web Design",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+    },
+    {
+      id: "2",
+      icon: "../assets/globe.png",
+      title: "Mobile Application",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg"
+    },
+    {
+      id: "3",
+      icon: "../assets/writing.png",
+      title: "Branding",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg"
+    }
+  ];
+
+  const handleclick = (prop) => {
+  prop === "left"
+    ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+    : setCurrentSlide(currentSlide <data.length-1 ? currentSlide + 1 : 0);
+}
   return (
     <div className="works" id="works">
-      <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src={mobile} />
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((item) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={item.icon} alt="m" />
+                  </div>
+                  <h2>{item.title}</h2>
+                  <p>{item.desc}</p>
+                  <span>Project</span>
                 </div>
-                <h2>Title</h2>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Tenetur perspiciatis, corporis consectetur provident velit
-                  earum alias maxime rerum!  
-                </p>
-                <span>Project</span>
+              </div>
+              <div className="right">
+                <img src={item.img} alt="" />
               </div>
             </div>
-            <div className="right">
-
-              <img src={blog} alt="" />
-            </div>
           </div>
-        </div>
+        ))}
       </div>
+      <img
+        src={arrow}
+        alt="a"
+        className="arrow left"
+        onClick={() => handleclick("left")}
+      />
+      <img
+        src={arrow}
+        alt="b"
+        className="arrow right"
+        onClick={() => handleclick()}
+      />
     </div>
   );
 };
 
-export default works;
+export default Works;
