876f65325470eb786f9c7b33c08ded33c9221703
Halil <78084765+halilkaran@users.noreply.github.com>
2022-05-08 19:36:49 +0200

    v24

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -10,21 +10,21 @@ const Works = () => {
       id: "1",
       icon: "../../assets/mobile.png",
       title: "Web Design",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      desc: "I have published many projects that I have done on netlify. I improved my React and JS skills with Bootcamp. ",
       img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
     },
     {
       id: "2",
       icon: "../assets/globe.png",
       title: "Mobile Application",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      desc: "I will lern too",
       img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg"
     },
     {
       id: "3",
       icon: "../assets/writing.png",
       title: "Branding",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      desc: " ",
       img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg"
     }
   ];
@@ -50,7 +50,12 @@ const Works = () => {
                   </div>
                   <h2>{item.title}</h2>
                   <p>{item.desc}</p>
-                  <span>Project</span>
+                  <span>
+                    {" "}
+                    <a href="https://app.netlify.com/teams/halilkarancontact/sites">
+                      Netlify
+                    </a>
+                  </span>
                 </div>
               </div>
               <div className="right">
