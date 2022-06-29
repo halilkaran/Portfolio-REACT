1fe763d8eb51b2f61b88e0d95883154afc6a3a02
Halil <78084765+halilkaran@users.noreply.github.com>
2022-06-29 12:58:12 +0200

    tobbar

--- src/components/topBar/Topbar.jsx
+++ src/components/topBar/Topbar.jsx
@@ -1,6 +1,8 @@
 import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
- 
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 const Topbar = ({ menuOpen, setMenuOpen }) => {
   return (
     <div className={"topbar " + (menuOpen && " active")}>
@@ -32,9 +34,8 @@ const Topbar = ({ menuOpen, setMenuOpen }) => {
             </span>
           </div>
           <div className="itemContainer">
-            <Mail className="icon" />
+            <Mail className="icon" style={{ color: "lightblue" }} />
             <span>
-              {" "}
               <a
                 style={{ textDecoration: "none", color: "black" }}
                 href="mailto:halilkarancontact@gmail.com"
@@ -42,7 +43,33 @@ const Topbar = ({ menuOpen, setMenuOpen }) => {
                 rel="noreferrer"
               >
                 halilkarancontact@gmail.com
-              </a>{" "}
+              </a>
+            </span>
+          </div>
+          <div className="itemContainer">
+            <LinkedInIcon className="icon" style={{ color: "#0A66C2" }} />
+            <span>
+              <a
+                style={{ textDecoration: "none", color: "black" }}
+                href="https://www.linkedin.com/in/halon-0294aa187/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Halil Karan LinkedIn
+              </a>
+            </span>
+          </div>
+          <div className="itemContainer">
+            <GitHubIcon className="icon" />
+            <span>
+              <a
+                style={{ textDecoration: "none", color: "black" }}
+                href="https://github.com/halilkaran"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Halil Karan Github
+              </a>
             </span>
           </div>
         </div>
